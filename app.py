1002e30914912487c96989dd504c2375ee2bfb6a
leandro <leandrors91@gmail.com>
2024-02-19 19:43:22 -0300

    removendo arquivo de treino exploratorio

--- app.py
+++ app.py
@@ -1,62 +1,86 @@
-# inicia data base
-db = [{
-    'id':1,
-    'nome':'Pular corda',
-    'descricao':'30 minutos',
-    'realizado':False,
-    'prioridade':'Alta'
-}]
+from flask import jsonify,make_response,Flask,request
+import mysql.connector
 
-# insert
-def insert(id,nome,descricao,realizado,prioridade):
-    atividade = {
-    'id': id,
-    'nome': nome,
-    'descricao': descricao,
-    'realizado': realizado,
-    'prioridade': prioridade
-}
-    db.append(atividade)
-    print('atividade inserida com sucesso')
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="MyUser",
+    password="MainPassword",
+    database="PYCODERBR",
+)
 
-# update
-def update(id,nome,descricao,realizado,prioridade):
-    for atividade in db:
-        if atividade['id']==id:
-            atividade |= {
-                'id':id,
-                'nome': nome,
-                'descricao': descricao,
-                'realizado': realizado,
-                'prioridade': prioridade
+atividades = ['preparar','mirar','atirar']
+
+app = Flask(__name__)
+app.json.sort_keys = False
+
+@app.route('/atividades',methods=['GET'])
+def get_atividade():
+    my_cursor = mydb.cursor()
+    my_cursor.execute('select * from atividades')
+    my_atividades = my_cursor.fetchall()
+    atividades = list()
+    for atividade in my_atividades:
+        atividades.append(
+            {
+                'id':atividade[0],
+                'nome':atividade[1],
+                'descricao':atividade[2],
+                'realizado':atividade[3],
+                'prioridade':atividade[4]
             }
-            break
-    print('atividade atualizada com sucesso')
+        )
+        
+    return make_response(jsonify(
+        mensagem='Lista de atividades',
+        atividades=atividades
+    ))
 
-# delete
-def delete(id):
-    for atividade in db:
-        if atividade['id']==2:
-            db.remove(atividade)
-            break
-    print('atividade deletada com sucesso')
+@app.route('/atividades',methods=['POST'])
+def post_atividade():
+    atividade = request.json
+    my_cursor = mydb.cursor()
+    sql = f'insert into atividades (nome,descricao,realizado,prioridade) values ("{atividade["nome"]}","{atividade["descricao"]}","{atividade["realizado"]}","{atividade["prioridade"]}")'
+    my_cursor.execute(sql)
+    mydb.commit()
+    return make_response(jsonify(
+        mensagem='Atividade adicionada com sucesso',
+        atividades=atividade
+    ))
+    
+@app.route('/atividades',methods=['DELETE'])
+def delete_atividade():
+    atividade = request.json
+    my_cursor = mydb.cursor()
+    sql = f'delete from atividades where id = {atividade["id"]}'
+    my_cursor.execute(sql)
+    mydb.commit()
+    return make_response(jsonify(
+        mensagem='Atividade deletada com sucesso',
+        atividades=atividade
+    ))
+    
+@app.route('/atividades',methods=['PUT'])
+def put_atividade():
+    atividade = request.json
+    my_cursor = mydb.cursor()
+    sql = f"UPDATE atividades SET nome='{atividade['nome']}', descricao='{atividade['descricao']}', realizado='{atividade['realizado']}', prioridade='{atividade['prioridade']}' WHERE id = {atividade['id']}"
+    my_cursor.execute(sql)
+    mydb.commit()
+    return make_response(jsonify(
+        mensagem='Atividade atualizada com sucesso',
+        atividades=atividade
+    ))
+    
+@app.route('/atividades',methods=['PATCH'])
+def patch_atividade():
+    atividade = request.json
+    my_cursor = mydb.cursor()
+    sql = f"UPDATE atividades SET realizado='{atividade['realizado']}' WHERE id = {atividade['id']}"
+    my_cursor.execute(sql)
+    mydb.commit()
+    return make_response(jsonify(
+        mensagem='Atividade atualizada com sucesso',
+        atividades=atividade
+    ))
 
-# select id
-def select(id):
-    item = None
-    for atividade in db:
-        if atividade['id']==id:
-            item = atividade
-    print(atividade)
-
-# list all
-def select_all():
-    print(db)
-
-select_all()
-insert(2,'Tomar whey','Pos treino',False,'Alta')
-select(2)
-update(2,'Tomar creatina','Pos treino',False,'Alta')
-select(2)
-delete(2)
-select_all()
+app.run(debug=True)
